fd59cc82beda0dfec82f56a3948b3d03d4507488
smaniren <renzosmania@hotmail.com>
2024-11-17 22:11:53 +0100

    updated environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,9 @@
 class Environment {
   production = false;
-  backendUrl = 'http://localhost:8080'; // dont forget to add the port!
+  url = 'http://localhost';
+  backendUrl = `${this.url}:8080`; // dont forget to add the port!
   apiUrl = `${this.backendUrl}/api`;
 }
 
 export const environment = new Environment();
+
